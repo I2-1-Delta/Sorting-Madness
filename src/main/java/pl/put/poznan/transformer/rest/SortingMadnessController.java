533830bb101a59f1090b3f04116e598273df3a1a
Milosz Matuszewski <myyylek@gmail.com>
2022-12-14 23:53:47 +0100

    Rest skeleton added

--- src/main/java/pl/put/poznan/transformer/rest/SortingMadnessController.java
+++ src/main/java/pl/put/poznan/transformer/rest/SortingMadnessController.java
@@ -1,46 +1,26 @@
 package pl.put.poznan.transformer.rest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.TextTransformer;
+import pl.put.poznan.transformer.sorting.SortingResult;
+
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/{text}")
-public class TextTransformerController {
+public class SortingMadnessController {
 
-    private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
-
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+    @GetMapping
+    public SortingResult sortIntegers(@RequestBody List<Integer> toSort){
+        return null;
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+    @GetMapping
+    public SortingResult sortObjects(@RequestBody List<Integer> toSort){
+        return null;
     }
-
-
-
 }
 
 
