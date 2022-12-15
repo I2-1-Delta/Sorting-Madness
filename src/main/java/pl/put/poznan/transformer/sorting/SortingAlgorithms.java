3597871485e0cba7b7569482afac1bdd6841488a
Milosz Matuszewski <myyylek@gmail.com>
2022-12-15 12:15:19 +0100

    Sorting algorithms enum added

---
+++ src/main/java/pl/put/poznan/transformer/sorting/SortingAlgorithms.java
@@ -0,0 +1,10 @@
+package pl.put.poznan.transformer.sorting;
+
+public enum SortingAlgorithms {
+    BubbleSort,
+    HeapSort,
+    InsertionSort,
+    MergeSort,
+    SelectionSort,
+    QuickSort
+}
