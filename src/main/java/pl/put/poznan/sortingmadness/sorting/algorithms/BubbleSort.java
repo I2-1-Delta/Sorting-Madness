68ec9941530bfc48c67a1daa07a0852a421429a2
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-01-14 16:47:26 +0100

    feat: bubble sort with limit

--- src/main/java/pl/put/poznan/sortingmadness/sorting/algorithms/BubbleSort.java
+++ src/main/java/pl/put/poznan/sortingmadness/sorting/algorithms/BubbleSort.java
@@ -8,6 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BubbleSort implements SortingStrategy {
+    private final int limit;
+
+    public BubbleSort() {
+        this(0);
+    }
+
+    public BubbleSort(int limit) {
+        this.limit = limit;
+    }
+
     @Override
     public String getName() {
         return "Bubble sort";
@@ -17,7 +27,12 @@ public class BubbleSort implements SortingStrategy {
     public List<Integer> sort(List<Integer> toSort) {
         List<Integer> result = new ArrayList<>(toSort);
         int n = result.size();
+        int numOfIterations = 0;
         for (int i = 0; i < n - 1; i++) {
+            numOfIterations++;
+            if (overLimit(numOfIterations)) {
+                break;
+            }
             for (int j = 0; j < n - i - 1; j++) {
                 if (result.get(j) > result.get(j + 1)) {
                     int temp = result.get(j);
@@ -34,7 +49,12 @@ public class BubbleSort implements SortingStrategy {
         List<JsonNode> result = new ArrayList<>(toSort);
         JsonNodeComparator comparator = new JsonNodeComparator(path);
         int n = result.size();
+        int numOfIterations = 0;
         for (int i = 0; i < n - 1; i++) {
+            numOfIterations++;
+            if (overLimit(numOfIterations)) {
+                break;
+            }
             for (int j = 0; j < n - i - 1; j++) {
                 if (comparator.compare(result.get(j), result.get(j + 1)) > 0) {
                     JsonNode temp = result.get(j);
@@ -44,5 +64,9 @@ public class BubbleSort implements SortingStrategy {
             }
         }
         return result;
+    }
+
+    private boolean overLimit(int numOfIterations) {
+        return limit != 0 && numOfIterations == limit;
     }
 }
--- src/test/java/pl/put/poznan/sortingmadness/sorting/BubbleSortTest.java
+++ src/test/java/pl/put/poznan/sortingmadness/sorting/BubbleSortTest.java
@@ -13,7 +13,7 @@ class BubbleSortTest {
     @Test
     void shouldSortIntegersInAscendingOrder() {
         List<Integer> integers = Fixtures.INT_LIST;
-        SortingStrategy sortingStrategy = new BubbleSort();
+        SortingStrategy sortingStrategy = new BubbleSort(0);
         List<Integer> result = sortingStrategy.sort(integers);
         assertThat(result).isSorted();
     }
@@ -21,8 +21,17 @@ class BubbleSortTest {
     @Test
     void shouldSortObjectsInAscendingOrder() {
         List<JsonNode> integers = Fixtures.OBJECT_LIST;
-        SortingStrategy sortingStrategy = new BubbleSort();
+        SortingStrategy sortingStrategy = new BubbleSort(0);
         List<JsonNode> result = sortingStrategy.sort(integers, Fixtures.PATH);
         assertThat(result).isSortedAccordingTo(new JsonNodeComparator(Fixtures.PATH));
+    }
+
+    @Test
+    void shouldStopAlgorithmWhenOverLimit() {
+        List<Integer> integers = Fixtures.INT_LIST;
+        SortingStrategy sortingStrategy = new BubbleSort(1);
+        List<Integer> result = sortingStrategy.sort(integers);
+        List<Integer> sortedWithoutLimit = new BubbleSort(0).sort(integers);
+        assertThat(result).doesNotContainSequence(sortedWithoutLimit);
     }
 }
