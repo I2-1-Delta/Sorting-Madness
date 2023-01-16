41c227a41f78eccca8ce348d9c30ead903cb90f3
michal-pawlicki <michalpawlicki02@gmail.com>
2023-01-16 14:47:13 +0100

    feat: insertion sort descending

--- src/main/java/pl/put/poznan/sortingmadness/sorting/algorithms/InsertionSort.java
+++ src/main/java/pl/put/poznan/sortingmadness/sorting/algorithms/InsertionSort.java
@@ -8,12 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InsertionSort implements SortingStrategy {
+    private final boolean descending;
 
+    public InsertionSort() {
+        this(false);
+    }
+
+    public InsertionSort(boolean descending) {
+        this.descending = descending;
+    }
     @Override
     public String getName() {
         return "Insertion sort";
     }
 
+    private boolean compare(Integer first, Integer second) {
+        if (descending) {
+            return first < second;
+        }
+        return first > second;
+    }
     @Override
     public List<Integer> sort(List<Integer> toSort) {
         List<Integer> result = new ArrayList<>(toSort);
@@ -21,7 +35,7 @@ public class InsertionSort implements SortingStrategy {
         for (int i = 1; i < n; i++) {
             int temp = result.get(i);
             int j = i - 1;
-            while (j >= 0 && result.get(j) > temp) {
+            while (j >= 0 && compare(result.get(j), temp)){
                 result.set(j + 1, result.get(j));
                 j = j - 1;
             }
@@ -29,7 +43,12 @@ public class InsertionSort implements SortingStrategy {
         }
         return result;
     }
-
+    private boolean compare(JsonNodeComparator comparator, JsonNode first, JsonNode second) {
+        if (descending) {
+            return comparator.compare(first, second) < 0;
+        }
+        return comparator.compare(first, second) > 0;
+    }
     @Override
     public List<JsonNode> sort(List<JsonNode> toSort, String path) {
         List<JsonNode> result = new ArrayList<>(toSort);
@@ -38,7 +57,7 @@ public class InsertionSort implements SortingStrategy {
         for (int i = 1; i < n; i++) {
             JsonNode temp = result.get(i);
             int j = i - 1;
-            while (j >= 0 && comparator.compare(result.get(j), temp) > 0) {
+            while (j >= 0 && compare(comparator, result.get(j), temp)) {
                 result.set(j + 1, result.get(j));
                 j = j - 1;
             }
