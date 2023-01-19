669b45013a44bdb15c9af797fb89f5d453d99e75
michal-pawlicki <michalpawlicki02@gmail.com>
2023-01-19 15:30:29 +0100

    feat: insertion sort iterations limit

--- src/test/java/pl/put/poznan/sortingmadness/sorting/InsertionSortTest.java
+++ src/test/java/pl/put/poznan/sortingmadness/sorting/InsertionSortTest.java
@@ -30,7 +30,7 @@ class InsertionSortTest {
     @Test
     void shouldSortIntegersInDescendingOrder() {
         List<Integer> integers = Fixtures.INT_LIST;
-        SortingStrategy sortingStrategy = new InsertionSort(true);
+        SortingStrategy sortingStrategy = new InsertionSort(true, 0);
         List<Integer> result = sortingStrategy.sort(integers);
         assertThat(result).isSortedAccordingTo(Comparator.reverseOrder());
     }
@@ -38,8 +38,17 @@ class InsertionSortTest {
     @Test
     void shouldSortObjectsInDescendingOrder() {
         List<JsonNode> integers = Fixtures.OBJECT_LIST;
-        SortingStrategy sortingStrategy = new InsertionSort(true);
+        SortingStrategy sortingStrategy = new InsertionSort(true, 0);
         List<JsonNode> result = sortingStrategy.sort(integers, Fixtures.PATH);
         assertThat(result).isSortedAccordingTo(new JsonNodeComparator(Fixtures.PATH).reversed());
+    }
+
+    @Test
+    void shouldStopAlgorithmWhenOverLimit() {
+        List<Integer> integers = Fixtures.INT_LIST;
+        SortingStrategy sortingStrategy = new InsertionSort(false, 1);
+        List<Integer> result = sortingStrategy.sort(integers);
+        List<Integer> sortedWithoutLimit = new InsertionSort(false, 0).sort(integers);
+        assertThat(result).doesNotContainSequence(sortedWithoutLimit);
     }
 }
