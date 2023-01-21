b580ffab6834cb8412c6a748acdeb2d688ba5733
miloszmat123 <myyylek@gmail.com>
2023-01-22 00:24:14 +0100

    filed in enum for descending added

--- src/main/java/pl/put/poznan/sortingmadness/logic/SortingStrategyEnum.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/SortingStrategyEnum.java
@@ -4,22 +4,28 @@ import pl.put.poznan.sortingmadness.sorting.SortingStrategy;
 import pl.put.poznan.sortingmadness.sorting.algorithms.*;
 
 public enum SortingStrategyEnum {
-    BUBBLE_SORT(new BubbleSort()),
-    HEAP_SORT(new HeapSort()),
-    INSERTION_SORT(new InsertionSort()),
-    MERGE_SORT(new MergeSort()),
-    SELECTION_SORT(new SelectionSort()),
-    QUICK_SORT(new QuickSort());
+    BUBBLE_SORT(new BubbleSort(), new BubbleSort(true)),
+    HEAP_SORT(new HeapSort(), new HeapSort(true)),
+    INSERTION_SORT(new InsertionSort(), new InsertionSort(true)),
+    MERGE_SORT(new MergeSort(), new MergeSort()),  //todo to implement descending in mergesort
+    SELECTION_SORT(new SelectionSort(), new SelectionSort()),  //todo to implement descending in selectionsort
+    QUICK_SORT(new QuickSort(), new QuickSort());  //todo to implement descending in quicksort
 
 
     private final SortingStrategy sortingStrategy;
+    private final SortingStrategy sortingStrategyDescending;
 
-    SortingStrategyEnum(SortingStrategy sortingStrategy) {
+    SortingStrategyEnum(SortingStrategy sortingStrategy, SortingStrategy sortingStrategyDescending) {
         this.sortingStrategy = sortingStrategy;
+        this.sortingStrategyDescending = sortingStrategyDescending;
     }
 
 
     public SortingStrategy getSortingStrategy() {
         return sortingStrategy;
+    }
+
+    public SortingStrategy getSortingStrategyDescending() {
+        return sortingStrategyDescending;
     }
 }
