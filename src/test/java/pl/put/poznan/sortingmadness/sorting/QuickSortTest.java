bdafcc3fc5811ab25d071f09d1aeaf5d75fd059c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-01-24 11:21:35 +0100

    test: Descending order in Quick Sort

--- src/test/java/pl/put/poznan/sortingmadness/sorting/QuickSortTest.java
+++ src/test/java/pl/put/poznan/sortingmadness/sorting/QuickSortTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Test;
 import pl.put.poznan.sortingmadness.sorting.algorithms.QuickSort;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,11 +28,38 @@ class QuickSortTest {
     }
 
     @Test
+    void shouldSortIntegersInDescendingOrder() {
+        List<Integer> integers = Fixtures.INT_LIST;
+        SortingStrategy sortingStrategy = new QuickSort(true, 0);
+        List<Integer> result = sortingStrategy.sort(integers);
+        assertThat(result).isSortedAccordingTo(Comparator.reverseOrder());
+    }
+
+    @Test
+    void shouldSortObjectsInDescendingOrder() {
+        List<JsonNode> integers = Fixtures.OBJECT_LIST;
+        SortingStrategy sortingStrategy = new QuickSort(true, 0);
+        List<JsonNode> result = sortingStrategy.sort(integers, Fixtures.PATH);
+        assertThat(result).isSortedAccordingTo(new JsonNodeComparator(Fixtures.PATH).reversed());
+    }
+
+    @Test
+    void shouldNotThrowIndexOutOfBoundWhenLastItemIsTheBigger() {
+        JsonNode object1 = Fixtures.toJsonNode("{\"name\": \"object\", \"sort\": {\"key\": 1}}");
+        JsonNode object2 = Fixtures.toJsonNode("{\"name\": \"object\", \"sort\": {\"key\": 2}}");
+        JsonNode object3 = Fixtures.toJsonNode("{\"name\": \"object\", \"sort\": {\"key\": 3}}");
+        List<JsonNode> objects = List.of(object2, object1, object3);
+        SortingStrategy sortingStrategy = new QuickSort();
+        List<JsonNode> result = sortingStrategy.sort(objects, Fixtures.PATH);
+        assertThat(result).isSortedAccordingTo(new JsonNodeComparator(Fixtures.PATH));
+    }
+
+    @Test
     void shouldStopAlgorithmWhenOverLimit() {
         List<Integer> integers = Fixtures.INT_LIST;
-        SortingStrategy sortingStrategy = new QuickSort(1);
+        SortingStrategy sortingStrategy = new QuickSort(false, 1);
         List<Integer> result = sortingStrategy.sort(integers);
-        List<Integer> sortedWithoutLimit = new QuickSort(0).sort(integers);
+        List<Integer> sortedWithoutLimit = new QuickSort(false, 0).sort(integers);
         assertThat(result).doesNotContainSequence(sortedWithoutLimit);
     }
 }
